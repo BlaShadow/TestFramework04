ca28f0366e73f342081eac44e775ddc8fa27b193
blashadow <blackzerogamer@gmail.com>
2018-09-20 10:34:23 -0400

    initial commit

---
+++ TestFramework04/TestFramework04/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  TestFramework04
+//
+//  Created by Blashadow on 9/20/18.
+//  Copyright © 2018 Blashadow. All rights reserved.
+//
+
+import Foundation
+
+class Service {
+    private init(){}
+    
+    public static func saySomething() -> String {
+        return "I am here"
+    }
+}
---
+++ TestFramework04/TestFramework04/TestFramework04.h
@@ -0,0 +1,19 @@
+//
+//  TestFramework04.h
+//  TestFramework04
+//
+//  Created by Blashadow on 9/20/18.
+//  Copyright © 2018 Blashadow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestFramework04.
+FOUNDATION_EXPORT double TestFramework04VersionNumber;
+
+//! Project version string for TestFramework04.
+FOUNDATION_EXPORT const unsigned char TestFramework04VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFramework04/PublicHeader.h>
+
+
